e78a272c958a9c104195e8f9dd0cb2d5781b74f8
AresOsredkar <aresosredkar1337@gmail.com>
2022-01-11 11:44:46 +0100

    adding bootstrap

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -13,38 +13,4 @@ $(document).ready(function(){
         $(this).css("z-index","auto");
         $(this).css("width","100%");
     });
-
-
-
-    //navigation
-    $("#home").click(function(){
-        window.location.href = "Home.html";
-    });
-    $("#bio").click(function(){
-        window.location.href = "Biography.html";
-    });
-    $("#school").click(function(){
-        window.location.href = "School.html";
-    });
-    $("#work").click(function(){
-        window.location.href = "Employment.html";
-    });
-    $("#contact").click(function(){
-        window.location.href = "Contact.html";
-    });
-
-    //clicable Contacts
-    $("#facebook").click(function(){
-        window.location.href = "https://facebook.com/ares_dregon";
-    });
-    $("#twitter").click(function(){
-        window.location.href = "https://twitter.com/aresgmv";
-    });
-    $("#youtube").click(function(){
-        window.location.href = "https://youtube.com/c/aresgmv";
-    });
-    $("#twitch").click(function(){
-        window.location.href = "https://twitch.tv/aresgmv";
-    });
-
 });
