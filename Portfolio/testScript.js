e0103f031b3dda97a1edbfc54e41aa91959022fd
AresOsredkar <aresosredkar1337@gmail.com>
2022-01-11 10:37:54 +0100

    Update

--- Portfolio/testScript.js
+++ Portfolio/testScript.js
@@ -1,35 +1,11 @@
 $(document).ready(function(){
 
-    $("nameError").hide();
-    var name = $("nameInput").val();
-    function nameValid(){
-        if(name==''){
-            $("nameError").show();
-            return false;
-        }
-        return true;
-    }
+    $(".img").hover(function(){
+        $(".img").addClass("imgAnimation");
+    });
+    $(".img").mouseleave(function(){
+        $(".img").removeClass("imgAnimation");
+    })
 
-    $("emailError").hide();
-    var regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,4}$/;
-    var email = $("email").val();
-    $("email").blur(function(){emailValid();})
-    function emailValid(){
-        if(regex.test(email)){
-            $("emailError").show();
-            return false;
-        }
-        return true;
-    }
 
-    $("telError").hide();
-    var tel = $("tel").val();
-    function telValid(){
-        if(name==''){
-            $("nameError").show();
-            return false;
-        }
-        return true;
-    }
-    
 });
