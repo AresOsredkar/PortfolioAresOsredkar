ee7e8b1b7dc2106aa0183779a03dce893a3480a4
AresOsredkar <aresosredkar1337@gmail.com>
2022-01-13 13:56:10 +0100

    Update JSminigame.js

--- Portfolio/script/JSminigame.js
+++ Portfolio/script/JSminigame.js
@@ -2,37 +2,48 @@ function Run(){
     //setup
     $(".minigame").attr("active","true");
     $("#start").attr("id","restart");
-    $(".snake").css("visibility","visible");
+    $(".snake").css({"visibility":"visible"});
+    $(".snake").css({"transition":"all 7s linear"});
     $("#restart").attr("onClick","Restart()");
     $("#restart").text("Restart");
     var Pos = { x: $(".minigame").width()/2 - 40 , y:$(".minigame").height() / 2 - 40}
     $(".snake").css({"top":Pos.y + "px","left":Pos.x + "px"});
     var keyP = "none";
-    var active = $(".minigame").attr("active");;
+    var active = $(".minigame").attr("active");
     var y = "y"
     $(".minigame").click(function(){
         $(".minigame").attr("active","true");
     });
     $(".minigame").mouseleave(function(){
-        $(".snake").css("background-color", "orange");
         $(".minigame").attr("active","false");
-        active = "false";
     });
+    var direction = { top: 0, left: 0}
     $(document).keyup(function(event){
         keyP = event.key;
-        if(keyP=="Escape"){
-            $(".minigame").attr("active","false");
-            $(".minigame").css("background-color","red");
+        console.log(keyP);
+        switch(keyP){
+            case "ArrowUp":
+                direction.top = "-100px";
+                direction.left = $(".snake").position().left
+                break;
+            case "ArrowDown":
+                direction.top = $(".minigame").height() + 100 + "px";
+                direction.left = $(".snake").position().left
+                break;
+            case "ArrowLeft":
+                console.log(direction.top);
+                direction.top = $(".snake").position().top;
+                console.log($(".snake").position().top);
+                direction.left = "-100px";
+                break;
+            case "ArrowRight":
+                direction.top = $(".snake").position().top;
+                direction.left = $(".minigame").width() + 100 + "px";
+                break;
         }
+        $(".snake").css({"top":direction.top, "left":direction.left});
     });
     var i = 0;
-    while(i < 4000 && active == "true"){
-        console.log(keyP + " "+ active);
-        i++;
-        active = $(".minigame").attr("active");
-    }
-    console.log("brken");
-
 }
 
 
