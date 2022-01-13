c5720e4647dcd92eb28b989c7b81293f2c904f7a
AresOsredkar <aresosredkar1337@gmail.com>
2022-01-13 13:05:00 +0100

    Most

--- Portfolio/script/JSminigame.js
+++ Portfolio/script/JSminigame.js
@@ -1,32 +1,46 @@
+function Run(){
+    //setup
+    $(".minigame").attr("active","true");
+    $("#start").attr("id","restart");
+    $(".snake").css("visibility","visible");
+    $("#restart").attr("onClick","Restart()");
+    $("#restart").text("Restart");
+    var Pos = { x: $(".minigame").width()/2 - 40 , y:$(".minigame").height() / 2 - 40}
+    $(".snake").css({"top":Pos.y + "px","left":Pos.x + "px"});
+    var keyP = "none";
+    var active = $(".minigame").attr("active");;
+    var y = "y"
+    $(".minigame").click(function(){
+        $(".minigame").attr("active","true");
+    });
+    $(".minigame").mouseleave(function(){
+        $(".snake").css("background-color", "orange");
+        $(".minigame").attr("active","false");
+        active = "false";
+    });
+    $(document).keyup(function(event){
+        keyP = event.key;
+        if(keyP=="Escape"){
+            $(".minigame").attr("active","false");
+            $(".minigame").css("background-color","red");
+        }
+    });
+    var i = 0;
+    while(i < 4000 && active == "true"){
+        console.log(keyP + " "+ active);
+        i++;
+        active = $(".minigame").attr("active");
+    }
+    console.log("brken");
+
+}
+
+
 $(document).ready(function(){
 
-    var headPos = { x: $(".minigame").width()/2 , y:$(".minigame").height() / 2}
-    var windowSize = { x: $(".minigame").width(), y: $(".minigame").height() };
-    var substraction = $(".minigame").position();
-    $(".minigame").click(
-        function(event){
-            //setup
-            $(".pause").css("visibility","hidden");
-            var left = false;
-            headPos.x = (event.pageX-substraction.left - 35) + "px";
-            headPos.y = (event.pageY-substraction.top - 35) + "px";
-            console.log("X: " + headPos.x + "; Y: " + headPos.y);
-            $(".snake").css({"top":headPos.y,"left":headPos.x});
-            
-            //move listener
-            $(".minigame").mousemove(function(event) {
-                headPos.x = (event.pageX-substraction.left - 35) + "px";
-                headPos.y = (event.pageY-substraction.top - 35) + "px";
-                $(".snake").css({"top":headPos.y,"left":headPos.x});
-                if (headPos.x < 0 || headPos.y < 0 || headPos.x > windowSize.x || windowSize.y){
-                    $(".pause").css("visibility","visible");
-                    left = true;
-                    return false;
-                }
-            });
-            if(left){
-                return false;
-            }
-        }
-    );
+
+
 });
+
+
+
