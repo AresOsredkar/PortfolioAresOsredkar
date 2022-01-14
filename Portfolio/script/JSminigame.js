1e019bd737871ae4aaaa8764d0d232125e869138
AresOsredkar <aresosredkar1337@gmail.com>
2022-01-14 14:23:11 +0100

    progree on agar

--- Portfolio/script/JSminigame.js
+++ Portfolio/script/JSminigame.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+    var activeWindow = false;
     $("#start").on("click",function(){
         Food();
+        activeWindow = true;
         $("#start").on("click");
         //setup
         $("#minigame").attr("active","true");
@@ -10,22 +12,15 @@ $(document).ready(function(){
         $("#restart").attr("onClick","Restart()");
         $("#restart").text("Restart");
         var Pos = { x: $("#minigame").width()/2 - 40 , y:$("#minigame").height() / 2 - 40}
-    
         $(".blob").css({"top":Pos.y + "px","left":Pos.x + "px"});
-    
-        var keyP = "none";
-    
-        var active = $("#minigame").attr("active");
-    
-        var y = "y";
-        $("#minigame").click(function(){
-            $("#minigame").attr("active","true");
-        });
-        $("#minigame").mouseleave(function(){
-            $("#minigame").attr("active","false");
-        });
+        return false;
+    });
 
-        var i = 0;
+    $("#minigame").on("mousemove",function(event){
+        if(activeWindow){
+
+        }
+        return false;
     });
 
 
@@ -36,9 +31,10 @@ $(document).ready(function(){
         var widt = $("#minigame").width()-40;
         var heigh = $("#minigame").height()-40;
         for(let i = 0; i<10;i++){     
-            eSpan = i + "' style='top:" + Math.floor(Math.random() * heigh) + "; left:" + Math.floor(Math.random() * widt) + "></span";
-            $("#minigame").appendTo("<" + sSpan+eSpan + ">");
-            console.log(i + " " + sSpan + eSpan);
+            eSpan = i + "' style=' top:" + Math.floor(Math.random() * heigh) + "px; left:" + Math.floor(Math.random() * widt) + "px'>OO</span";
+            var span = "<" + sSpan + eSpan + ">";
+            $("#minigame").append(span);
+            console.log(i + " " + span);
         }
         return false; 
     }
