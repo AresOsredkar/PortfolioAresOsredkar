6e14af5cff947f69fd9b668f43336982953d1c2c
AresOsredkar <aresosredkar1337@gmail.com>
2022-01-20 16:51:15 +0100

    Fixed miniggame movement

--- Portfolio/script/JSminigame.js
+++ Portfolio/script/JSminigame.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
         $("#minigame").attr("active","true");
         $("#start").attr("id","restart");
         $(".blob").css({"visibility":"visible"});
-        $(".blob").css({"transition":"all 7s linear"});
         $("#restart").attr("onClick","Restart()");
         $("#restart").text("Restart");
         var Pos = { x: $("#minigame").width()/2 - 40 , y:$("#minigame").height() / 2 - 40}
@@ -17,9 +16,15 @@ $(document).ready(function(){
     });
 
     $("#minigame").on("mousemove",function(event){
-        if(activeWindow){
-
+        var mousePos = { 
+            x: event.clientX,
+            y: event.clientY 
         }
+        var actualPos = {
+            x: mousePos.x - $("#minigame").offset().left - 40,
+            y: mousePos.y - $("#minigame").offset().top - 80
+        }
+        $(".blob").css({"top":actualPos.y,"left":actualPos.x});
         return false;
     });
 
@@ -30,8 +35,9 @@ $(document).ready(function(){
         var eSpan = "";
         var widt = $("#minigame").width()-40;
         var heigh = $("#minigame").height()-40;
-        for(let i = 0; i<10;i++){     
-            eSpan = i + "' style=' top:" + Math.floor(Math.random() * heigh) + "px; left:" + Math.floor(Math.random() * widt) + "px'>OO</span";
+        for(let i = 0; i<10;i++)
+        {     
+            eSpan = i + "' style=' top:" + Math.floor(Math.random() * heigh) + "px; left:" + Math.floor(Math.random() * widt) + "px'>O</span";
             var span = "<" + sSpan + eSpan + ">";
             $("#minigame").append(span);
             console.log(i + " " + span);
